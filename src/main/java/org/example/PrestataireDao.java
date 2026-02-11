633b5797dd7186d8186f32f7fb1e8d93cc195d63
hibaHAJAJI <wiamhiba08@gmail.com>
2026-02-11 14:50:48 +0100

    premier parti

--- src/main/java/org/example/PrestataireDao.java
+++ src/main/java/org/example/PrestataireDao.java
@@ -25,55 +25,5 @@ public class PrestataireDao {
         }
     }
 
-    public void suprimerPrestataire(Connection con){
-        System.out.println("===Suprimer un Prestataire ===");
-        System.out.println("Entrer id supprimer");
-        int id=sc.nextInt();
-        sc.nextLine();
-        try(PreparedStatement ps= con.prepareStatement("DELETE From Prestataires WHERE id =?")) {
-          ps.setInt(1,id);
-          int remove=ps.executeUpdate();
-          if (remove>0){
-              System.out.println("Prestataire est suprimé !");
-          }else {
-              System.out.println("Aucun prestataire pour suprimer");
-          }
 
-        }catch (Exception e){
-            System.out.println("Error"+e.getMessage());
-        }
-    }
-    public void recherchPrestataire(Connection con){
-        System.out.println("===Rechercher un Prestataire ===");
-    }
-    public void modifierPrestataire(Connection con){
-        System.out.println("===Modifier un Prestataire ===");
-        System.out.println("Entrer le nom modifier : ");
-        String nom= sc.nextLine();
-        System.out.println("Entrer l'adress modifier :");
-        String adress=sc.nextLine();
-        try(PreparedStatement ps= con.prepareStatement("UPDATE Prestataire SET nom = ? , adress = ? WHERE id =?")) {
-           ps.setString(1,nom);
-           ps.setString(2,adress);
-           ps.setInt(3,id);
-           int mod =ps.executeUpdate();
-           if(mod>0){
-               System.out.println("Modifier success !!");
-           }else {
-               System.out.println("");
-           }
-        }
-
-    }
-    public void listerPrestataire(Connection con){
-        System.out.println("===Lister un Prestataire ===");
-        System.out.println("Entrer l' id affichage :");
-        int id = sc.nextInt();
-        sc.nextLine();
-        try(PreparedStatement ps= con.prepareStatement("SELECT * FROM Prestataires")) {
-            ps.setString();
-
-        }
-
-    }
 }
