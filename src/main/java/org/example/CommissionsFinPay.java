bec501b2fe730851672ccb7f43d5566c7c5c09a6
hibaHAJAJI <wiamhiba08@gmail.com>
2026-02-12 17:58:33 +0100

    commission

--- src/main/java/org/example/CommissionsFinPay.java
+++ src/main/java/org/example/CommissionsFinPay.java
@@ -1,5 +1,30 @@
 package org.example;
 
 public class CommissionsFinPay {
+     private int id;
+     private static final double taux =0.02;
 
+     public static double calculauto(double montant){
+       return montant*taux;
+     }
+
+    public CommissionsFinPay(int id, double taux) {
+        this.id = id;
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTaux() {
+        return taux;
+    }
+
+    public void setTaux(int taux) {
+    }
 }
