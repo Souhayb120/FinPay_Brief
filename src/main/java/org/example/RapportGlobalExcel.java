15bae3d2ff0f94a2f3be64489d3e8d4bfa83b3bb
hibaHAJAJI <wiamhiba08@gmail.com>
2026-02-19 16:11:03 +0100

    swivi excel

--- src/main/java/org/example/RapportGlobalExcel.java
+++ src/main/java/org/example/RapportGlobalExcel.java
@@ -8,65 +8,62 @@ import java.io.IOException;
 import java.sql.*;
 
 public class RapportGlobalExcel {
-    private RapportGlobalExcel(){
 
-    }
-    
+    public static void excecute() throws SQLException {
 
-    public static  void  excecute(){
+        String sql = "SELECT DATE_FORMAT(facture.date_facture, '%Y-%m') AS mois,\n" +
+                " prestataire.nom AS prestataire,\n" +
+                "COUNT(facture.id) AS nombre_factures,\n " +
+                "SUM(facture.montant_total) AS total_genere,\n " +
+                "SUM(facture.montant_total)*0.02 AS total_commission \n" +
+                "FROM prestataire \n" +
+                "JOIN facture ON facture.id_prestataire = prestataire.id \n" +
+                "GROUP BY DATE_FORMAT(facture.date_facture,'%Y-%m'), prestataire.nom \n" +
+                "ORDER BY mois;\n";
+        Connection conn = DatabaseConnection.getConnection();
+        PreparedStatement ps = conn.prepareStatement(sql);
+
+        ResultSet rs = ps.executeQuery();
         //xlsx file
-        Workbook workbook=new XSSFWorkbook();
+        Workbook workbook = new XSSFWorkbook();
         //name of the sheet
-        Sheet sheet=workbook.createSheet("My product sheet");
+        Sheet sheet = workbook.createSheet("Rapport Global");
 
-
-
-        generateHeaderRow(sheet, workbook);
-        setColumnWidts(sheet);
-        // name of the file generated
-        writeFile(workbook);
-
-    }
-
-    private static void setColumnWidts(Sheet sheet) {
-        sheet.setColumnWidth(0,7_000);
-        sheet.setColumnWidth(1,8_000);
-        sheet.setColumnWidth(2,8_000);
-        sheet.setColumnWidth(3,9_000);
-
-    }
-
-    private static void generateHeaderRow(Sheet sheet, Workbook workbook) {
+        // HEADER
         Row headerRow = sheet.createRow(0); // 0 is the index of the first row
-        String[] headers ={"Prestataire","Nombre Factures","Total Généré","Total Commissions"};
-        for(int i =0; i<headers.length;i++){
-            Cell cell=headerRow.createCell(i);
-            cell.setCellValue(headers[i]);
-            cell.setCellStyle(getCellStyle(workbook));
-        }
-    }
-    private static CellStyle getCellStyle(Workbook workbook) {
+        String[] headers = {"Prestataire", "Nombre Factures", "Total Généré", "Total Commissions"};
+        //style
         CellStyle headerStyle = workbook.createCellStyle();
         Font boldFont = workbook.createFont();
         boldFont.setBold(true);// make font bold
         headerStyle.setFont(boldFont);
-        // sets cell to grey background
-        headerStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
-        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
-        // horizontally aligns the text
-        headerStyle.setAlignment(HorizontalAlignment.CENTER);
-        return headerStyle;
-    }
-    private static void writeFile(Workbook workbook) {
-        try {
-            FileOutputStream fileOutputStream =new FileOutputStream("rapportglobalmois.xlsx");
-            workbook.write(fileOutputStream);
-            workbook.close();
-        }catch (IOException e){
-            e.printStackTrace();
+        for (int i = 0; i < headers.length; i++) {
+            Cell cell = headerRow.createCell(i);
+            cell.setCellValue(headers[i]);
+            cell.setCellStyle(headerStyle);
+            sheet.setColumnWidth(i, 8000);
         }
+        //DATA
+        int rowsIndex = 1;
+        while (rs.next()) {
+            Row row = sheet.createRow(rowsIndex++);
+            row.createCell(0).setCellValue(rs.getString("prestataire"));
+            row.createCell(1).setCellValue(rs.getInt("nombre_factures"));
+            row.createCell(2).setCellValue(rs.getDouble("total_genere"));
+            row.createCell(3).setCellValue(rs.getDouble("total_commission"));
+        }
+try {
+    FileOutputStream fileOutputStream = new FileOutputStream("rapportglobalmois.xlsx");
+    workbook.write(fileOutputStream);
+    workbook.close();
+    fileOutputStream.close();
+} catch (Exception e) {
+    e.printStackTrace();
+}
+
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws SQLException {
         RapportGlobalExcel.excecute();
     }
 }
