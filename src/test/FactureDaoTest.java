60c052ef676e5209a10887e9c3d91780bc37721a
HibaHAJAJI <wiamhiba08@gmail.com>
2026-02-26 16:12:50 +0100

    Rename test methods for clarity

--- src/test/FactureDaoTest.java
+++ src/test/FactureDaoTest.java
@@ -8,15 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class FactureDaoTest {
 
     @Test
-    void verifierPAID() {
+    void verifierMontantPayee() {
         assertEquals("PAID", FactureDao.verifierFactureStatus(500,500));
     }
     @Test
-    void verifierPENDING() {
+    void verifierMontantPartiel() {
         assertEquals("PENDING", FactureDao.verifierFactureStatus(300,800));
     }
     @Test
-    void verifier() {
+    void verifierMontantNull() {
         assertEquals("PENDING", FactureDao.verifierFactureStatus(0,500));
     }
 }
